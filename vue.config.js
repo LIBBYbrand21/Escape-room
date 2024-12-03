6950121e04e5bf945d796672a57496ed9414aa6c
LibbyBrand21 <m0583284644@gmail.com>
2024-12-03 18:13:06 +0200

    initial commit

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,17 @@
-const { defineConfig } = require("@vue/cli-service");
+/*const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
-  transpileDependencies: true,
-});
+  transpileDependencies: true
+})*/
+module.exports = {
+  pwa: {
+    name: 'Escape Room',
+    short_name: 'escape room',
+    themeColor: 'white',
+    msTileColor: '#000000',
+    manifestOptions: {
+      start_url: '.',
+      display: 'fullscreen',
+      background_color: '#FFFFFF',
+    },
+  },
+};
